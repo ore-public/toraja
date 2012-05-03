a37ad2deab3574e2bacae2e6e0fc131763f87fea
masaya <masaya@Lenovo.(none)>
2012-05-03 22:24:34 +0900

    oiax coffee script 入門第0回完了の状態

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toraja::Application.config.secret_token = 'ed2d7594486d02cae795fdffed6b33429a9b309f7170e7adca4f0f73bb49ae9bede4ed38de8634a31f6d1c35110608fc29ce33316ab4c6b9afdab88bd72ce536'
